b22e206adc1e7c7a78ae5d93e3238d233f00e224
Oliver Kalmend <okalmend@microsoft.com>
2017-02-22 12:05:39 +0200

    blop

--- main_oliver.py
+++ main_oliver.py
@@ -1,5 +1,4 @@
 import numpy as np
-import random
 
 class Pizza:
     def __init__(self):
@@ -16,12 +15,6 @@ class Slice:
         self.h = H
         self.w = W
 
-    def area(self):
-        return self.h * self.w
-
-    def conflicts_with(self, other):
-        return True
-
 def pizza_parser(filename):
     pizza = Pizza()
     with open(filename) as f:
@@ -30,15 +23,11 @@ def pizza_parser(filename):
     return pizza
 
 def slice_printer(slices):
-    print 'selected area ' + str(selected_area(slices))
     outputfile = open('result.out', 'w')
     outputfile.write(str(len(slices)) + '\n')
     for slice in slices:
-        outputfile.write(" ".join ([str (slice.x), str (slice.y), str (slice.x + slice.h - 1), str (slice.y + slice.w - 1), '\n']))
+        outputfile.write(" ".join ([str (slice.x), str (slice.y), ":" ,str (slice.x +slice.w - 1), str (slice.y + slice.h - 1), '\n']))
     outputfile.close()
-
-def selected_area (slices):
-    return sum ([s.area() for s in slices])
 
 def check_valid(slice, min_required):
     m_count = 0
@@ -60,26 +49,32 @@ def get_valid_slices(rows, cols, slices, pizza):
             if check_valid(pizza.lines[i:i+rows][j:j+cols], pizza.mini):
                 slices.append(Slice(i, j, rows, cols))
 
-def remove_conflicts(all_slices, selected_slice):
-    new_slices = []
-    for s in all_slices:
-        if not selected_slice.conflicts_with (s):
-            new_slices.append(s)
-    return new_slices
 
-def select_slices_random(all_slices):
-    selected_slices = []
+def getValidSliceFromLocation(x,y, pizza):
+    for y_end in range(y+1,y+getMaxPossibleHeight(pizza.rows,pizza.maxc,y)):
+        for x_end in range(x+getMaxPossibleWidth(pizza.cols, pizza.maxc, x)-1, 0, -1):
+            if check_valid(pizza.lines[y:y_end + 1, x:x_end + 1], pizza.mini):
+                return Slice(x, y, y_end - y+ 1,x_end - x + 1)
+    return None
 
-    while all_slices:
-        selected_pos = random.choice(range(0, len (all_slices)))
-        selected_slice = all_slices[selected_pos]
-        selected_slices.append(selected_slice)
-        all_slices.pop(selected_pos)
-        all_slices = remove_conflicts (all_slices, selected_slice)
+def getMaxPossibleWidth(cols, max_size, x):
+    if cols - x < max_size:
+        return cols - x
+    else:
+        return max_size
 
-    print "Random pick selected " + str(len(selected_slices)) + " slices with area " + \
-          str(selected_area(selected_slices))
-    return selected_slices
+
+def getMaxPossibleHeight(rows, max_size, y):
+    if rows - y < max_size:
+        return rows - y
+    else:
+        return max_size
+
+def conflicts_with_troubled(slice, troublemakers):
+    for trouble in troublemakers:
+        if slice.
+
+
 
 def main():
     pizza = pizza_parser('small.in')
@@ -115,8 +110,8 @@ def main():
 
 
 
-    # random_slices = select_slices_random (slices)
-
     slice_printer(slices)
+
+
 
 main()

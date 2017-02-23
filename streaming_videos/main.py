3b3a2017046385e5e7319638b14447cd18f8080f
cage <cagehub@github.com>
2017-02-23 20:08:02 +0200

    input parsin

---
+++ streaming_videos/main.py
@@ -0,0 +1,18 @@
+class Endpoint:
+    def __init__(self, dc_latency, latencies):
+        self.dc_latency = dc_latency
+        self.latencies = latencies # list of tuples of id and latency e.g. (1, 500)
+
+class Request:
+    def __init__(self, count, video_id, endpoint_id):
+        self.count = count
+        self.video_id = video_id
+        self.endpoint_id = endpoint_id
+
+videos = [] # list of sizes
+endpoints = []
+requests = []
+
+cache_size = 0
+cache_count = 0
+

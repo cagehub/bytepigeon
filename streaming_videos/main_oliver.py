272a4221eae5e8072755f265428edebdb4c4cf66
Oliver Kalmend <okalmend@microsoft.com>
2017-02-24 00:43:41 +0200

    Scorer added

--- streaming_videos/main_oliver.py
+++ streaming_videos/main_oliver.py
@@ -51,15 +51,15 @@ def parser(filename):
     return d
 
 
-def print_results(caches):
-    out_file = open("result.out", "w")
+def print_results (caches):
+    out_file = open ("result.out", "w")
     out_file.write(str(len(caches)))
     out_file.write("\n")
 
     for c in caches:
         out_file.write(str(c.id))
         for v in c.videos:
-            out_file.write(" " + str(v))
+            out_file.write(" " + str (v))
         out_file.write("\n")
     out_file.close()
 
@@ -97,6 +97,31 @@ def getSorter(item):
 def getBiggestCacheLoss(lossCostArray):
     return max(lossCostArray, key=attrgetter('cost'))
 
+
+def getSaved(data, count, endpoint_id, video_id):
+    best_saved = 0
+    ep = data.endpoints[endpoint_id]
+    for cacheId in ep.latencies:
+        if(video_id in data.caches[cacheId].videos):
+            latency = ep.latencies[cacheId]
+            saved = count * (ep.dc_latency - latency)
+            if saved > best_saved:
+                 best_saved = saved
+    return best_saved
+
+def calculateScore(data):
+    score = 0
+    req_count = 0
+    for request in data.requests:
+        req_count += request.count
+        score += getSaved(data, request.count, request.endpoint_id, request.video_id)
+    return score*1000/req_count
+
+
+
+
+
+
 def main():
     data = parser('streaming_videos/me_at_the_zoo.in')
     for video_id in range(0,data.videos.__len__()):
@@ -106,4 +131,52 @@ def main():
     data.maxLossCosts.sort(key=lambda x: x.cost, reverse=True)
     print(' x '.join(str(e.cost) for e in data.maxLossCosts))
 
+    caches = []
+    cachesMem = [data.cache_size]* data.cache_count
+    totalMem = data.cache_count * data.cache_size
+    res_i = 0
+
+    skiplist = []
+    while res_i < data.maxLossCosts.__len__():
+        vidSize = data.videos[data.maxLossCosts[res_i].videoId]
+        if data.maxLossCosts[res_i].cost == 0 or totalMem < vidSize:
+            print("cost 0, exiting")
+            break
+
+        if cachesMem[data.maxLossCosts[res_i].cacheId] >= vidSize:
+            #print("insert")
+            cachesMem[data.maxLossCosts[res_i].cacheId] -= vidSize
+            totalMem -= vidSize
+            data.caches[data.maxLossCosts[res_i].cacheId].videos.append(data.maxLossCosts[res_i].videoId)
+            skiplist.append(res_i)
+
+        res_i +=1
+
+    print("out")
+
+    res_i = 0
+    while res_i < data.maxLossCosts.__len__():
+        vidSize = data.videos[data.maxLossCosts[res_i].videoId]
+        if data.maxLossCosts[res_i].cost == 0 or totalMem < vidSize:
+            print("out of mems")
+            break
+
+        for i in range(0,data.cache_count):
+            if cachesMem[i] >= vidSize:
+                cachesMem[i] -= vidSize
+                totalMem -= vidSize
+                if(data.maxLossCosts[res_i].videoId in data.caches[i].videos):
+                    continue
+                data.caches[i].videos.append(data.maxLossCosts[res_i].videoId)
+
+        res_i += 1
+
+    print("done")
+    print_results(data.caches)
+    print(calculateScore(data))
 main()
+
+#class Cache:
+#    def __init__(self, id, videos):
+ #       self.id = id
+ #       self.videos = videos

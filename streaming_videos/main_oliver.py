bcdb8baa66a87c6e6d0a622a8e1e32c03d5acbc5
Oliver Kalmend <okalmend@microsoft.com>
2017-02-23 20:56:08 +0200

    pseudo bullshit oliver

--- streaming_videos/main_oliver.py
+++ streaming_videos/main_oliver.py
@@ -1,13 +1,56 @@
+class Latency:
+    def __init__(self, ep_id, latency):
+        self.endpoint_id = ep_id
+        self.latency = latency
+
+
 class Endpoint:
     def __init__(self, dc_latency, latencies):
         self.dc_latency = dc_latency
-        self.latencies = latencies # list of tuples of id and latency e.g. (1, 500)
+        self.latencies = latencies  # list of Latency
+
 
 class Request:
-    def __init__(self, count, video_id, endpoint_id):
-        self.count = count
+    def __init__(self, video_id, endpoint_id, count):
         self.video_id = video_id
         self.endpoint_id = endpoint_id
+        self.count = count
+
+
+class Cache:
+    def __init__(self, size, videos):
+        self.videos = videos
+
+
+class Data:
+    videos = []  # list of sizes
+    endpoints = []
+    requests = []
+    cache_size = 0
+    cache_count = 0
+
+
+def parser(filename):
+    d = Data()
+    with open(filename) as f:
+        _, endp, reqs, d.cache_count, d.cache_size = map(int, f.readline().split())
+        d.videos = map(int, f.readline().split())
+
+        for _ in range(endp):
+            dcl, cnt = map(int, f.readline().split())
+            latencies = []
+            for _ in range(cnt):
+                ep_id, latency = map(int, f.readline().split())
+                latencies.append(Latency(ep_id, latency))
+            d.endpoints.append(Endpoint(dcl, latencies))
+
+        for _ in range(reqs):
+            video_id, endpoint_id, count = map(int, f.readline().split())
+            d.requests.append(Request(video_id, endpoint_id, count))
+    return d
+
+
+data = parser('me_at_the_zoo.in')
 
 class LossCost:
     def __init__(self, cost, cacheId):
@@ -27,11 +70,15 @@ Ymul = 3
 Zmul = 5
 
 maxLossCosts = []
-
+lossCostArray = []
 
 def getCacheLossCost_CacheArray(video):
-    
-    endPointsWithVideo =
+    for request in requests:
+        cacheId = 0
+        while cacheId < cache_count:
+            lossCostArray[]
+
+    lossCostArray[]
     return (LossCost(10,2), LossCost(5,3), LossCost(232,30))
 
 def getSorter(item):
@@ -42,6 +89,7 @@ def getMaxLossCost(lossCost_CacheArray):
 
 def main():
     for video in videos:
+
         maxLossCosts.append(getMaxLossCost(getCacheLossCost_CacheArray(video)))
     print(maxLossCosts[0].cost)
 main()
